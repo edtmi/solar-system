2ee1afcc267c0e7058f533a2aabed9d20630c6d3
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-10-29 18:10:28 -0300

    requisito 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
@@ -7,6 +9,15 @@ class Missions extends Component {
       <>
         <div data-testid="missions" />
         <Title headline="Missões" />
+        {missions.map((value) => (
+          <MissionCard
+            key={ value.name }
+            name={ value.name }
+            year={ value.year }
+            country={ value.country }
+            destination={ value.destination }
+          />
+        ))}
       </>
     );
   }
