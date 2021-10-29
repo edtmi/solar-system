38c63f587ba55e3b1f081271db562e4c3cd1e193
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2021-10-29 17:22:42 -0300

    requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
@@ -7,6 +9,13 @@ class SolarSystem extends Component {
       <>
         <div data-testid="solar-system" />
         <Title headline="Planetas" />
+        {planets.map((value) => (
+          <PlanetCard
+            key={ value.name }
+            planetName={ value.name }
+            planetImage={ value.image }
+          />
+        ))}
       </>
     );
   }
