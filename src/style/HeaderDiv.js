cd21211968fd087767695030227f2016431cb793
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-26 11:58:49 -0300

    :sparkles: creating header style

---
+++ src/style/HeaderDiv.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const HeaderDiv = styled.header`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+
+  h1{
+    border-bottom: 2px solid white;
+    color: white;
+    font-weight: normal;
+    font-size: 52px;
+    padding: 0.5em 0.5em 0.25em;
+  }
+`;
+
+export default HeaderDiv;
