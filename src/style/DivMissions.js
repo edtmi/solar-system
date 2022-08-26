aae0a2bf72142ea697bb1d1efadfe1bbbe86be0f
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-26 20:43:58 -0300

    :sparkles: creating divmissions style

---
+++ src/style/DivMissions.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const DivMissions = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: auto;
+  text-align: center;
+  width: 60%;
+`;
+
+export default DivMissions;
