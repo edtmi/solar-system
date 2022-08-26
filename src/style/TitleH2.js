2854a36d41efbead8b65e59fd238ab21c1f2b02c
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-26 12:12:28 -0300

    :sparkles: styling titleh2 component

---
+++ src/style/TitleH2.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const TitleH2 = styled.h2`
+    color: white;
+    font-family: 'Poiret One', sans-serif;
+    font-weight: normal;
+    font-size: 32px;
+    padding: 0.5em 0.5em 0.25em;
+    text-align: center;
+`;
+
+export default TitleH2;
