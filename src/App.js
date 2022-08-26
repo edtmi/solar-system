2b5c8624551e68c440b6359b9fdf87266bc9fa80
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-26 15:36:28 -0300

    :sparkles: removing trailing space

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import BackgroundImg from './components/BackgroundImg';
-import SolarSystem from './components/DivPlanets';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <BackgroundImg /> 
+        <BackgroundImg />
         <Header />
         <SolarSystem />
         <Missions />
