7049d720d1de10ee4d65889c27d8f6d2b5bf756c
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-26 15:38:36 -0300

    :sparkles: creating divplanetssection style

---
+++ src/style/DivPlanetsSection.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const DivPlanetsSection = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  margin: auto;
+  text-align: center;
+  width: 90%;
+
+`;
+
+export default DivPlanetsSection;
