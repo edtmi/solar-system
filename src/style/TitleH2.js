deabcc37cbecdd42ebbc769c51ab83083f0be018
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-26 15:37:42 -0300

    :sparkles: removing property font-family on titleh2

--- src/style/TitleH2.js
+++ src/style/TitleH2.js
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 
 const TitleH2 = styled.h2`
     color: white;
-    font-family: 'Poiret One', sans-serif;
-    font-weight: normal;
     font-size: 32px;
+    font-weight: normal;
     padding: 0.5em 0.5em 0.25em;
     text-align: center;
 `;
