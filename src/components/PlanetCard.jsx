05f21872eb2002bf91be534af91b859de436ba3f
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-26 15:41:12 -0300

    :build: import divplanetsimg in planetcard component

--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import DivPlanetsImg from '../style/DivPlanetsImg';
 
 class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <DivPlanetsImg data-testid="planet-card">
         <h3 data-testid="planet-name">
           {planetName}
         </h3>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
-      </div>
+      </DivPlanetsImg>
     );
   }
 }
