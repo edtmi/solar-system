f98a655039ce39f2b2137db65252e60272805b7f
Eduardo Miyazaki <eduardo.tatsuo@hotmail.com>
2022-08-26 11:16:47 -0300

    :sparkles: import backgroundimg component and globalstyle tye style to render

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+import BackgroundImg from './components/BackgroundImg';
 
 class App extends React.Component {
   render() {
     return (
       <>
+        <BackgroundImg />
         <Header />
         <SolarSystem />
         <Missions />
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './style/globals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+      <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
